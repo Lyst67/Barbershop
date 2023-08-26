734af750dcd7d243ccd8879fbed54de3b28886c6
Lyst67 <p_listopad@ukr.net>
2023-08-26 22:59:47 +0300

    mob-ancher

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,15 +1,27 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('[data-menu-open]'),
-    closeMenuBtn: document.querySelector('[data-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
+    openMenuBtn: document.querySelector(".js-menu-open"),
+    closeMenuBtn: document.querySelector(".js-menu-close"),
+    menu: document.querySelector(".js-mob-menu"),
   };
 
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
-    document.body.classList.toggle('no-scroll');
+    refs.menu.classList.toggle("is-hidden");
+    document.body.classList.toggle("no-scroll");
+    refs.menu.classList.add("is-open");
   }
+
+  // Add event listener for all anchor links on the page
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", (event) => {
+      // Check if the mobile menu is open
+      if (refs.menu.classList.contains("is-open")) {
+        // Close the mobile menu
+        toggleMenu();
+      }
+    });
+  });
 })();
--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,22 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelectorAll(".js-modal-open"),
+    closeModalBtn: document.querySelector(".js-modal-close"),
+    modal: document.querySelector(".js-modal"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.forEach((element) => {
+    element.addEventListener("click", toggleModal);
+  });
   refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  //* Закривання по кліку на бекдроп
+  refs.modal.addEventListener("click", removeModal);
+  function removeModal(e) {
+    if (e.target === e.currentTarget) {
+      refs.modal.classList.add("is-hidden");
+    }
+  }
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
